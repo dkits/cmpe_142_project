bd60ee658476a9442904ca9b3943085df20c65cf
dkits <derickL12@yahoo.com>
2018-04-14 12:25:12 -0700

    Sample website code pushed

---
+++ cmake-build-debug/CMakeFiles/cmpe142_project.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cmpe142_project.dir/main.cpp.obj"
+  "cmpe142_project.pdb"
+  "cmpe142_project.exe"
+  "cmpe142_project.exe.manifest"
+  "libcmpe142_project.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cmpe142_project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
